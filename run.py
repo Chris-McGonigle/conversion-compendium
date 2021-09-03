22acd0ee883831fcf824213c059172474c21ee99
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-09-03 16:51:42 +0000

    Add function to display conversion based on unit and value inputs

--- run.py
+++ run.py
@@ -29,31 +29,31 @@ class Temperature:
     def convert_temp():
         """
         Function to convert temperatures. Function first checks if selection
-        is what the user intended. If not it loops back to main menu.
+        is what the user intended. If not it loops back to main menu. 
+        It then takes the user input which is then passed through 
+        the calculation function
         """
         print("\nYou have selected Temperature. Is this correct?")
 
         confirm_type = input("\nY/N:").upper()
 
         if confirm_type == ("Y"):
-            print("\nSelect starting unit in Celsius, Farenheit or Kelvin")
-            temp_selection = input("Unit Type (C,F or K):").upper()
-            temp_value = float(input("\nPlease enter the temperature to convert:"))
+            print("\nSelect starting unit in Celsius, Fahrenheit or Kelvin")
+            self.temp_selection = input("Unit Type (C,F or K):").upper()
+            self.temp_value = float(input("\nPlease enter the temperature to convert:"))
+            results_temp()
 
         elif confirm_type == ("N"):
             main_menu()
 
+    def results_temp():
+        """
+        Function to return the results of the temperature conversion to the terminal
+        """
 
-def convert_length():
-    print("worked2")
+        if (temp_selection == "C"):
+            print(f"\n {temp_value} in Celsius converts to:")
 
-
-def convert_volume():
-    print("worked3")
-
-
-def convert_currrency():
-    print("worked4")
 
 
 main_menu()
