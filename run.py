d8a567e242df4c920c6acb62b139b705ea2dcbef
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-09-03 14:39:10 +0000

    Add paths for main menu display selection

--- run.py
+++ run.py
@@ -7,15 +7,25 @@ def main_menu():
     print("MAIN MENU")
     print("")
     print("Please enter the number of the conversion you would like to carry out")
-    print("-" * 79)
+    print("-" * 69)
     print("1. Temperature")
     print("2. Length")
     print("3. Volume")
     print("4. Currency")
 
     selection = input("Please enter your selection here:")
-    print(selection)
+
+    if selection == ("1"):
+        convert_temp()
+    elif selection == ("2"):
+        convert_length()
+    elif selection == ("3"):
+        convert_volume()
+    elif selection == ("4"):
+        convert_currrency()
+
+def convert_temp():
+    print("worked")
 
 main_menu()
-
 
