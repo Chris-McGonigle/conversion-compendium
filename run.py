6f5141fe6594c4364a17736bf6a1f2083bacd31d
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2021-09-04 14:35:53 +0000

    Add celsius to kelvin conversion result

--- run.py
+++ run.py
@@ -17,20 +17,14 @@ def main_menu():
 
     if selection == ("1"):
         Temperature.convert_temp()
-    elif selection == ("2"):
-        convert_length()
-    elif selection == ("3"):
-        convert_volume()
-    elif selection == ("4"):
-        convert_currrency()
 
 
 class Temperature:
     def convert_temp():
         """
         Function to convert temperatures. Function first checks if selection
-        is what the user intended. If not it loops back to main menu. 
-        It then takes the user input which is then passed through 
+        is what the user intended. If not it loops back to main menu.
+        It then takes the user input which is then passed through
         the calculation function
         """
         print("\nYou have selected Temperature. Is this correct?")
@@ -39,21 +33,20 @@ class Temperature:
 
         if confirm_type == ("Y"):
             print("\nSelect starting unit in Celsius, Fahrenheit or Kelvin")
-            self.temp_selection = input("Unit Type (C,F or K):").upper()
-            self.temp_value = float(input("\nPlease enter the temperature to convert:"))
-            results_temp()
-
+            temp_selection = input("Unit Type (C,F or K):").upper()
+            temp_value = float(input("\nPlease enter the temperature to convert:"))
+            if temp_selection == ("C"):
+                print(f"\n{temp_value}{temp_selection} converted equals:")
+                print(f"\n{Temperature.celsius2kelvin(temp_value)} Kelvin")
         elif confirm_type == ("N"):
             main_menu()
 
-    def results_temp():
+    def celsius2kelvin(temp_value):
         """
-        Function to return the results of the temperature conversion to the terminal
+        Conversion formula for Celsius to Kelvin
         """
-
-        if (temp_selection == "C"):
-            print(f"\n {temp_value} in Celsius converts to:")
-
-
+        c2k = temp_value + 273.15
+        return c2k
 
 main_menu()
+
